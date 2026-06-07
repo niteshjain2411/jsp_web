e2ff5ee45e131eb6c42e75d87bef4897861a460e
Nitesh Jain <nitesh.jain2411@gmail.com>
2026-06-07 16:33:05 +0530

    Delete method correction. Jinvani video included.

--- src/main/java/org/jsp/config/FirebaseConfig.java
+++ src/main/java/org/jsp/config/FirebaseConfig.java
@@ -4,16 +4,12 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
-
-    @Value("${firebase.project-id:}")
-    private String firebaseProjectId;
 
     @PostConstruct
     public void initialize() {
